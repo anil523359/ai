6901962b499bf5ff25191d3e9c54daf359ae5fb3
Anil Kumar <135340964+anil523359@users.noreply.github.com>
2024-08-14 17:14:56 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pm2 stop all
-pm2 delete all
+
+# Path to the PM2 process file
+PM2_PROCESS_FILE="/var/www/applications/ozak/ecosystem.config.js"
+
+# Stop the application
+if pm2 list | grep -q 'ozak'; then
+    echo "Stopping application..."
+    pm2 stop "$PM2_PROCESS_FILE" --name "ozak"
+    pm2 delete "$PM2_PROCESS_FILE" --name "ozak"
+else
+    echo "Application is not running."
+fi
