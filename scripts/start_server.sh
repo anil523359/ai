5d06bfe28954cc9f838e87191883770a457ecae9
Anil Kumar <135340964+anil523359@users.noreply.github.com>
2024-08-14 17:13:03 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-cd /var/www/applications/ozak
-pm2 serve build/ 3000 --name "Ozakai-Web" --spa
+# Path to the application directory
+APP_DIR="/var/www/applications/ozak"
+
+# Path to the PM2 process file
+PM2_PROCESS_FILE="$APP_DIR/ecosystem.config.js"
+
+# Check if the app is already running
+if pm2 list | grep -q 'ozak'; then
+    echo "Application is already running. Restarting..."
+    pm2 restart "$PM2_PROCESS_FILE" --name "ozak"
+else
+    echo "Application is not running. Starting..."
+    pm2 start "$PM2_PROCESS_FILE" --name "ozak"
+fi
